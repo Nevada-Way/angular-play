3e123703360fd7c33b071758ae766ed4a6afe457
Nevada-Way <95190960+Nevada-Way@users.noreply.github.com>
2023-07-26 17:24:09 +0300

    4 files modified

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-comp1',
   templateUrl: './comp1.component.html',
-  styleUrls: ['./comp1.component.scss']
+  styleUrls: ['./comp1.component.scss'],
 })
 export class Comp1Component {
+  myObservable$ = new Observable<string>((subscriber) => {
+    console.log('Observable executed');
+    subscriber.next('Alice');
+    setTimeout( () => subscriber.next('Ben'), 2000);
+    setTimeout( () => subscriber.next('Charlie'), 4000);
+  });
 
+  ngOnInit() {
+    console.log('Triggering a subscribe to myObservable');
+    
+    const mySubs = this.myObservable$.subscribe((value: string) => console.log(value));
+    
+    setTimeout( () => 
+    {
+      console.log('Unsubscribed from myObservable');
+      mySubs.unsubscribe()}
+      , 3000);
+  }
 }
