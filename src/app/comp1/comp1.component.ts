2faa61ed4e6d5e99ddd677d10017298f3d498798
Nevada-Way <95190960+Nevada-Way@users.noreply.github.com>
2023-07-26 18:40:25 +0300

    1 file modified

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -10,19 +10,22 @@ export class Comp1Component {
   myObservable$ = new Observable<string>((subscriber) => {
     console.log('Observable executed');
     subscriber.next('Alice');
-    setTimeout( () => subscriber.next('Ben'), 2000);
-    setTimeout( () => subscriber.next('Charlie'), 4000);
+    setTimeout(() => subscriber.next('Ben'), 2000);
+    setTimeout(() => subscriber.next('Charlie'), 4000);
   });
 
   ngOnInit() {
-    console.log('Triggering a subscribe to myObservable');
-    
-    const mySubs = this.myObservable$.subscribe((value: string) => console.log(value));
-    
-    setTimeout( () => 
-    {
-      console.log('Unsubscribed from myObservable');
-      mySubs.unsubscribe()}
-      , 3000);
+    console.log('Triggering subscribe 1 to myObservable');
+    this.myObservable$.subscribe((value: string) =>
+      console.log('Subs 1 value = ', value)
+    );
+
+    setTimeout(
+      () =>
+        this.myObservable$.subscribe((value: string) =>
+          console.log('Subs 2 value = ', value)
+        ),
+      1000
+    );
   }
 }
