fdbfd0f66ebe0d91eb81d17627a6bc5d765c0e7e
Nevada-Way <95190960+Nevada-Way@users.noreply.github.com>
2023-07-29 15:12:03 +0300

    1 file modified

--- src/app/comp2/comp2.component.ts
+++ src/app/comp2/comp2.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
+import { EMPTY, Observable, timeout } from 'rxjs';
 
 @Component({
   selector: 'app-comp2',
@@ -8,21 +8,31 @@ import { EMPTY, Observable } from 'rxjs';
 })
 export class Comp2Component {
   ngOnInit() {
-    const myObservable1$ = new Observable(() =>
-      console.log('myObservable1 executed.')
-    );
     const myObservable2$ = new Observable<string>((subscriber2) => {
       console.log('myObservable2 executed.');
+
       subscriber2.next('Alice');
+
+      setTimeout(() => {
+        subscriber2.next('Ben');
+      }, 2000);
+
+      setTimeout(() => {
+        subscriber2.error(new Error('Testing error'));
+      }, 3000);
+
+      return () => {
+        console.log('This is the Teardown section.');
+      };
     });
 
-    // console.log('Before subscribe to myObservable1');
-    // myObservable1$.subscribe();
-    // console.log('After subscribe to myObservable1');
-    ///
     console.log('Before subscribe to myObservable2');
-    myObservable2$.subscribe();
-    console.log('After subscribe to myObservable2');
+    myObservable2$.subscribe({
+      next: (value) => console.log('Value emiited is = ', value),
+      error: (err) => console.log(err.message),
+      complete: () => console.log('myObservable2 COMPLETED.'),
+    });
 
+    console.log('After subscribe to myObservable2');
   }
 }
